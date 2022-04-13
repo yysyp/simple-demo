887cb5c325a32cf6bb152700f9ff8a2c6ae2e781
yysyp <yysyp@foxmail.com>
2022-04-13 19:20:14 +0800

    add h2

--- src/main/java/ps/demo/WebServerApplication.java
+++ src/main/java/ps/demo/WebServerApplication.java
@@ -1,9 +1,12 @@
 package ps.demo;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.sql.DataSource;
 //import ps.demo.util.SwingTool;
 
 @Slf4j
@@ -15,7 +18,6 @@ public class WebServerApplication { //implements CommandLineRunner {
         long totalMemory = Runtime.getRuntime().totalMemory();
         //long freeMemory = Runtime.getRuntime().freeMemory();
         log.info("--->>maxMemory={}, totalMemory={}, usedMemory={}", maxMemory/1024/1024, totalMemory/1024/1024, (maxMemory-totalMemory)/1024/1024);
-
         SpringApplication.run(WebServerApplication.class, args);
 
 //        SpringApplication springApplication = new SpringApplication(WebServerApplication.class);
--- src/main/java/ps/demo/controller/HealthzController.java
+++ src/main/java/ps/demo/controller/HealthzController.java
@@ -2,21 +2,27 @@ package ps.demo.controller;
 
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import ps.demo.dto.response.HealthzResponse;
 
+import javax.sql.DataSource;
+
 @Slf4j
 @Controller
 @RequestMapping("/healthz")
 public class HealthzController {
 
+    @Autowired
+    DataSource dataSource;
 
     @GetMapping
     @ResponseBody
     public HealthzResponse health() {
+        log.info("simple-demo datasource={}", dataSource);
         HealthzResponse healthzResponse = new HealthzResponse();
 
         healthzResponse.setData(new HealthzResponse.Data(HealthzResponse.Status.UP));
