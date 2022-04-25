3f075c0ad7e2b9a7695367a6091458db8e2ddb67
yysyp <yysyp@foxmail.com>
2022-04-25 13:44:34 +0800

    remove icon click lib

--- bathsbc-trim.bat
+++ bathsbc-trim.bat
@@ -5,6 +5,8 @@ del /f src\test\java\ps\demo\util\SeleniumTest2.java
 del /f src\test\java\ps\demo\util\Tess4JTest.java
 del /f src\main\java\ps\demo\util\SwingTool.java
 del /f src\main\java\ps\demo\util\MyWebDriverUtil.java
+del /f src/main/java/pslab/MonkeyRecordPlay.java
+del /f src/main/java/pslab/NativeHookDemo.java
 
 del pom.xml
 ren pom-small.xml pom.xml
