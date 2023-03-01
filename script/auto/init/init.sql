6b946cca90fc546c23a8ee689f0da54aa55f93b9
yysyp <yysyp@foxmail.com>
2023-03-01 19:54:11 +0800

    auto script milestone1

--- script/auto/init/init.sql
+++ script/auto/init/init.sql
@@ -1,4 +1,8 @@
+#CREATE DATABASE IF NOT EXISTS `testdb` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+#grant all PRIVILEGES on testdb.* to test@'%' identified by 'test';
+#flush privileges;
 use testdb;
+
 CREATE TABLE IF NOT EXISTS student_details (
  roll_no int(10) NOT NULL,
  registration_no int(10) NOT NULL AUTO_INCREMENT,
@@ -8,9 +12,7 @@ CREATE TABLE IF NOT EXISTS student_details (
  marks int(3) NOT NULL DEFAULT '0',
  gender varchar(6) NOT NULL DEFAULT 'male',
  PRIMARY KEY(registration_no)
-) ENGINE=InnoDB;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
 
 INSERT INTO `student_details` (`roll_no`,`registration_no`,`first_name`,`last_name`,`class`,`marks`,`gender`) VALUES (1,100,'aaa','s','1',1,'1');
 INSERT INTO `student_details` (`roll_no`,`registration_no`,`first_name`,`last_name`,`class`,`marks`,`gender`) VALUES (2,101,'bbb','x','1',2,'1');
-
-commit;
