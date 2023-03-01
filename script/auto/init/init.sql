b7f6b36ea3ad220f09d9eb2a9e8ab65655aca4b7
yysyp <yysyp@foxmail.com>
2023-03-01 17:00:43 +0800

    rename remove sh

---
+++ script/auto/init/init.sql
@@ -0,0 +1,16 @@
+use testdb;
+CREATE TABLE IF NOT EXISTS student_details (
+ roll_no int(10) NOT NULL,
+ registration_no int(10) NOT NULL AUTO_INCREMENT,
+ first_name varchar(50) NOT NULL DEFAULT '',
+ last_name varchar(50) DEFAULT '',
+ class varchar(10) NOT NULL DEFAULT '',
+ marks int(3) NOT NULL DEFAULT '0',
+ gender varchar(6) NOT NULL DEFAULT 'male',
+ PRIMARY KEY(registration_no)
+) ENGINE=InnoDB;
+
+INSERT INTO `student_details` (`roll_no`,`registration_no`,`first_name`,`last_name`,`class`,`marks`,`gender`) VALUES (1,100,'aaa','s','1',1,'1');
+INSERT INTO `student_details` (`roll_no`,`registration_no`,`first_name`,`last_name`,`class`,`marks`,`gender`) VALUES (2,101,'bbb','x','1',2,'1');
+
+commit;
